34d992eada8cf4cf01b15afea85a228b32b06bb4
Hugo-Val <hvb1507@hotmail.com>
2023-05-01 00:43:20 -0600

    adios

--- PI-Countries/client/src/App.js
+++ PI-Countries/client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Route, Routers } from "react-router-dom";
+import { Route } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Routers>
-        <Route path="/" element={LandingPage} ></Route>
-      </Routers>
+      
+      <Route exact path="/" component={LandingPage} />
+      
       </div>
     
   );
